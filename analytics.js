a14e609c4c09c9fb1118cbe80403206fd695db36
German Attanasio <germanattanasio@gmail.com>
2018-06-12 17:16:57 -0300

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,18 @@
+var digitalData = {
+  page: {
+    pageInfo: {
+      analytics: {
+        category: "Demo (Watson Platform)",
+      }
+    }
+  }
+};
+
+function loadAnalytics() {
+  var idaScript = document.createElement('script');
+  idaScript.src = '//www.ibm.com/common/stats/ida_stats.js';
+  document.head.appendChild(idaScript);
+}
+
+
+window.addEventListener('load', loadAnalytics);
